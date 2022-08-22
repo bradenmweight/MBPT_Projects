1d8cfc85e452fa0bbef88f189bcf8a6438be436e
Eric Rodolfo Koessler <ekoessle@bluehive.bluehive.circ.private>
2022-08-22 15:45:23 -0400

    fixed sbatch

--- 2_Electron_Square_Well/sbatch.py
+++ 2_Electron_Square_Well/sbatch.py
@@ -4,9 +4,12 @@
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --mem-per-cpu=2GB
 #SBATCH -t 1:00:00
-#SBATCH --output=main.out
-#SBATCH --error=main.err
+#SBATCH --output=main_file.out
+#SBATCH --error=main_file.err
 
-import main
+import os
+import sys
+sys.path.append(os.popen("pwd").read().replace("\n",""))
+from main import main
 
-main.main()
+main()
