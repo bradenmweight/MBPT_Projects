ecdaf0464bfd42b348d8d62bea3f12a7387eec17
bradenmweight <braden.m.weight@gmail.com>
2022-08-24 15:27:15 -0400

    Changed output and error file name in sbatch

--- 2_Electron_Square_Well/sbatch.py
+++ 2_Electron_Square_Well/sbatch.py
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --mem-per-cpu=2GB
 #SBATCH -t 1:00:00
-#SBATCH --output=main.out
-#SBATCH --error=main.err
+#SBATCH --output=output.out
+#SBATCH --error=output.err
 
 import main
 
