5fa9e746b2e968f02f66fe755e19720b0da29a44
bradenmweight <braden.m.weight@gmail.com>
2022-08-24 16:28:09 -0400

    SBATCH output and error files

--- 2_Electron_Square_Well/sbatch.py
+++ 2_Electron_Square_Well/sbatch.py
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --mem-per-cpu=2GB
 #SBATCH -t 1:00:00
-#SBATCH --output=main_file.out
-#SBATCH --error=main_file.err
+#SBATCH --output=output.out
+#SBATCH --error=output.err
 
 import os
 import sys
